31c0930f18ec95b47b807122af350ea13d847d09
Kashif Salman <1268871+ksalman@users.noreply.github.com>
2021-07-14 14:19:15 -0700

    Update get_folders
    * Move the code that makes api request to a new method which, hopefully,
    will be used by other methods in the future
    
    * Redo docstring style

--- thycotic/api.py
+++ thycotic/api.py
@@ -8,10 +8,11 @@ from thycotic import Folder
 
 
 class Api:
-    def __init__(self, username, password, url):
+    def __init__(self, username, password, url, verify=True):
         self.username = username
         self.password = password
         self.url = url
+        self.verify = verify
         self._get_token()
 
     def _get_token(self):
@@ -30,45 +31,43 @@ class Api:
         ACCESS_TOKEN = response.json()["access_token"]
         s.headers.update({"Authorization": "Bearer {}".format(ACCESS_TOKEN)})
         self._session = s
+        self._session.verify = self.verify
 
-    def get_folders(self, parentfolder=None, skip=None, take=None):
+    def get_folders(
+        self, parentfolder=None, permissionrequired="View", skip=None, take=None
+    ):
         """Search, filter, sort, and page secret folders
 
         The caller is responsible for handling pages and looping to gather all
         of the data
 
-        Args:
-          parentfolder:
-            Parent folder ID [optional]
-          skip:
-            Number of records to skip before taking results [optional]
-
-        Returns:
-          hasNext:
-            Whether there are any results in additional pages [bool]
-          hasPrev:
-            Whether there are any results in previous pages [bool]
-          nextSkip:
-            Correct value of 'skip' for the next page of results
-          prevSkip::
-            Correct value of 'skip' for the previous page of results
-          pageCount:
-            Number of result pages available with current query options
+        :param parantfolder: (optional) Parent folder ID
+        :param permissionrequired: (optional) Specify whether to filter by Owner, Edit,
+            AddSecret, View folder permission. Default is View
+        :param skip: (optional) Number of recoreds to skip taking results
+        :param take: (optional) Maximum number of records to include in results
+        :returns: PagingOfFolderSummary
 
         """
 
         endpoint = "/folders"
         params = {
             "filter.parentFolderId": parentfolder,
+            "filter.permissionRequired": permissionrequired,
             "skip": skip,
             "take": take,
         }
-        response = self._session.get(
-            self._geturl(endpoint),
-            params=params,
-            verify=False,
-        )
-        return [Folder(**x) for x in response.json()["records"]]
+        return self._get(self._geturl(endpoint), params=params)
+
+    def _get(self, url, **kwargs):
+        return self._internal_call("GET", url, kwargs)
+
+    def _internal_call(self, method, url, params):
+        args = dict(params=params)
+        args["params"] = args["params"]["params"]
+        response = self._session.request(method, url, **args)
+        response.raise_for_status()
+        return response.json()
 
     def _geturl(self, endpoint):
         return urljoin(self.url, API_URI) + endpoint
