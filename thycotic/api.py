770aa73850db09887eba5145f536e18721a994b7
Kashif Salman <1268871+ksalman@users.noreply.github.com>
2021-07-15 21:17:34 -0700

    Remove the `next()` method
    The method makes things more complicated than it needs to be.

--- thycotic/api.py
+++ thycotic/api.py
@@ -31,9 +31,7 @@ class Api:
             {"Authorization": "Bearer {}".format(self._token["access_token"])}
         )
 
-    def get_folders(
-        self, parentfolder=None, permissionrequired="View", skip=None, take=None
-    ):
+    def get_folders(self, parentfolder=None, permissionrequired="View", limit=10):
         """Search, filter, sort, and page secret folders
 
         The caller is responsible for handling pages and looping to gather all
@@ -42,8 +40,8 @@ class Api:
         :param parantfolder: (optional) Parent folder ID
         :param permissionrequired: (optional) Specify whether to filter by Owner, Edit,
             AddSecret, View folder permission. Default is View
-        :param skip: (optional) Number of recoreds to skip taking results
-        :param take: (optional) Maximum number of records to include in results
+        :param limit: (optional) Maximum number of records to include in results.
+            Default is 10
         :returns: PagingOfFolderSummary
 
         """
@@ -52,38 +50,15 @@ class Api:
         params = {
             "filter.parentFolderId": parentfolder,
             "filter.permissionRequired": permissionrequired,
-            "skip": skip,
-            "take": take,
+            "take": limit,
         }
-        return self._get(self._geturl(endpoint), endpoint=endpoint, params=params)
-
-    def _get(self, url, **kwargs):
-        return self._internal_call("GET", url, kwargs)
+        return self._internal_call("GET", self._geturl(endpoint), params=params)
 
     def _internal_call(self, method, url, params):
-        endpoint = params.pop("endpoint")
         args = dict(params=params)
-        args["params"] = args["params"]["params"]
         response = self._session.request(method, url, **args)
         response.raise_for_status()
-        mydict = response.json()
-        mydict["endpoint"] = endpoint
-        return mydict
+        return response.json()
 
     def _geturl(self, endpoint):
         return urljoin(self.url, self.API_URI) + endpoint
-
-    def next(self, result):
-        """Helper method to handle pagination"""
-
-        if not ("endpoint" or "filter") in result:
-            return None
-        if result["hasNext"] == False:
-            return None
-        endpoint = result["endpoint"]
-        params = {}
-        for k in result["filter"]:
-            params[f"filter.{k}"] = result["filter"][k]
-        params["skip"] = result["skip"] + result["take"]
-        params["take"] = result["take"]
-        return self._get(self._geturl(endpoint), endpoint=endpoint, params=params)
